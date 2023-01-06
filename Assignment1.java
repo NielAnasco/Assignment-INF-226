3b4f901cad15ed1b0081438f4ecb34a3e3703600
NielAnasco <122077231+NielAnasco@users.noreply.github.com>
2023-01-06 09:15:45 +0000

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -2,10 +2,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.Scanner;
 
-public class birthday {
+public class Assignment1 {
     public static void main(String[] args) {
 
-        // Java program to check if a student number is valid
+// BIRTHDAY
+
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter your Birthday: ");
         String studentBirthday = scan.nextLine();
@@ -22,8 +23,42 @@ public class birthday {
         } else {
             System.out.println("invalid birthday");
         }
+// EMAIL
 
-        
+           Scanner scan1 = new Scanner(System.in);
+           System.out.print("Enter Email Address: ");
+           String studentEmail = scan.nextLine();
+       
+   
+           // Long method
+           Pattern pattern1 = Pattern.compile("\\w+@students.national-u.edu.ph");
+           Matcher matcher1 = pattern.matcher(studentEmail);
+   
+           boolean match1 = matcher.matches();
+   
+           if (match) {
+               System.out.println("valid email address");
+           } else {
+               System.out.println("invalid email address");
+           }
+   
+// TELEPHONE
 
-    }
+           Scanner scan2 = new Scanner(System.in);
+           System.out.print("Enter Telephone Number: ");
+           String studentNumber = scan.nextLine();
+       
+   
+           // Long method
+           Pattern pattern2 = Pattern.compile("\\+63\\d{10}");
+           Matcher matcher2 = pattern.matcher(studentNumber);
+   
+           boolean match2 = matcher.matches();
+   
+           if (match) {
+               System.out.println("valid telephone number");
+           } else {
+               System.out.println("invalid telephone number");
+           }
+        }
 }
