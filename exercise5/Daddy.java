0e996775dd8b1414c953e8eabf25f93b59fb6d8f
NielAnasco <122077231+NielAnasco@users.noreply.github.com>
2023-02-10 14:45:12 +0800

    Add files via upload

---
+++ exercise5/Daddy.java
@@ -0,0 +1,8 @@
+public class Daddy {
+    protected String surname = "Añasco";
+
+    protected void showSurname() {
+        System.out.println("My family name is: " + surname);
+    }
+}
+
---
+++ exercise5/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+
+    private String name;
+
+    // get method
+    public String getName() {
+        return this.name;
+    }
+    // set method
+    public void setName(String personName) {
+        this.name = personName;
+    } 
+}
